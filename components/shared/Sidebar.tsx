2f2879c1ebfb9ea3797eb729ec71b2b5ca2baa66
AnjoC07 <anjocaluya.ama@gmail.com>
2024-02-18 15:08:28 +0800

    fix: use client error

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,8 +1,18 @@
+"use client";
+
+import { navLinks } from "@/constants";
+import { SignedIn } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+// Import the usePathname hook from next/navigation
+import { usePathname } from "next/navigation";
+
+// Rest of your imports
 
 const Sidebar = () => {
+  // Use usePathname inside a Client Component
+  const pathname = usePathname();
+
   return (
     <aside className="sidebar">
       <div className="flex size-full flex-col gap-4">
@@ -14,6 +24,28 @@ const Sidebar = () => {
             height={28}
           />
         </Link>
+        <nav className="sidebar-nav">
+          <SignedIn>
+            <ul className="sidebar-nav_elements">
+              {navLinks.map((link) => {
+                const isActive = link.route === pathname;
+
+                return (
+                  <li
+                    key={link.route}
+                    className={`sidebar-nav_element ${
+                      isActive
+                        ? "bg-purple-gradient text-white"
+                        : "text-gray-700"
+                    }`}
+                  >
+                    {link.label}
+                  </li>
+                );
+              })}
+            </ul>
+          </SignedIn>
+        </nav>
       </div>
     </aside>
   );
