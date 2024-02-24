7f931cf35f1637825214f0147a4b56a427edf416
AnjoC07 <anjocaluya.ama@gmail.com>
2024-02-24 14:14:21 +0800

    build: Submit Button  @components\shared\TransformationForm.tsx

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -47,11 +47,15 @@ const TransformationForm = ({
   userId,
   type,
   creditBalance,
+  config = null,
 }: TransformationFormProps) => {
   const transformationType = transformationTypes[type];
   const [Image, setImage] = useState(data);
   const [newTransformation, setnewTransformation] =
     useState<Transformations | null>(null);
+  const [isSubmitting, setisSubmitting] = useState(false);
+  const [isTransforming, setIsTransforming] = useState(false);
+  const [transformationConfig, setTransformationConfig] = useState(config);
 
   const initialValues =
     data && action === "Update"
@@ -174,6 +178,14 @@ const TransformationForm = ({
             )}
           </div>
         )}
+
+        <Button
+          type="submit"
+          className="submit-button capitalize"
+          disabled={isSubmitting}
+        >
+          Submit
+        </Button>
       </form>
     </Form>
   );
