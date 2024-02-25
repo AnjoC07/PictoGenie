9275bb43ba4caf03a118915c755dcb8de2a41404
AnjoC07 <anjocaluya.ama@gmail.com>
2024-02-25 10:39:17 +0800

    create: components/shared/MediaUploader.tsx

---
+++ components/shared/MediaUploader.tsx
@@ -0,0 +1,9 @@
+import { useToast } from "@/components/ui/use-toast";
+
+const MediaUploader = () => {
+  const { toast } = useToast();
+
+  return <div>MediaUploader</div>;
+};
+
+export default MediaUploader;
