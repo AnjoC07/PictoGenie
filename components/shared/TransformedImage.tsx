e9d837617bf416c9e22eafb4a0267ec348687448
AnjoC07 <anjocaluya.ama@gmail.com>
2024-02-25 16:22:18 +0800

    add: download button components\shared\TransformedImage.tsx

--- components/shared/TransformedImage.tsx
+++ components/shared/TransformedImage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import Image from "next/image";
 
 const TransformedImage = ({
   image,
@@ -9,7 +9,29 @@ const TransformedImage = ({
   setIsTransforming,
   hasDownload = false,
 }: TransformedImageProps) => {
-  return <div>TransformedImage</div>;
+  const downloadHandler = () => {};
+
+  return (
+    <div className="flex flex-col gap-4">
+      <div>
+        <div className="flex-between">
+          <h3 className="h3-bold text-dark-600">Transformed</h3>
+
+          {hasDownload && (
+            <button className="download-btn" onClick={downloadHandler}>
+              <Image
+                src="/assets/icons/download.svg"
+                alt="download"
+                width={24}
+                height={24}
+                className="pb-[6px]"
+              />
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TransformedImage;
