947ed7ce360f4b703bf39fac80c6569009fdc997
AnjoC07 <anjocaluya.ama@gmail.com>
2024-02-25 14:48:51 +0800

    create: onUploadErrorHandler  components/shared/MediaUploader.tsx

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -1,9 +1,27 @@
 import { useToast } from "@/components/ui/use-toast";
+import { CldUploadWidget } from "next-cloudinary";
 
 const MediaUploader = () => {
   const { toast } = useToast();
+  const onUploadSuccessHandler = (result: any) => {};
 
-  return <div>MediaUploader</div>;
+  const onUploadErrorHandler = (result: any) => {
+    toast({
+      title: "Something went wrong while uploading",
+      description: "Please try again",
+      duration: 5000,
+      className: "error-toast",
+    });
+  };
+
+  return (
+    <CldUploadWidget
+      uploadPreset="pictogenie"
+      options={{ multiple: false, resourceType: "image" }}
+      onSuccess={onUploadSuccessHandler}
+      onError={onUploadErrorHandler}
+    ></CldUploadWidget>
+  );
 };
 
 export default MediaUploader;
