d2600f06e0dc5b7427d1a40afca9f139c3e50d4e
AnjoC07 <anjocaluya.ama@gmail.com>
2024-02-27 10:11:10 +0800

    add: add image to db  @lib\actions\image.actions.ts

---
+++ lib/actions/image.actions.ts
@@ -0,0 +1,18 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { connectToDatabase } from "../database/mongoose";
+import { handleError } from "../utils";
+
+// ADD IMAGE TO DATABASE
+export async function addImage({ image, userId, path }: AddImageParams) {
+  try {
+    await connectToDatabase();
+
+    revalidatePath(path);
+
+    return JSON.parse(JSON.stringify(image));
+  } catch (error) {
+    handleError(error);
+  }
+}
