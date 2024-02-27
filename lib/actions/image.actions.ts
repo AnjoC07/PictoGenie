eb6949e649197a86c901056de682a4f998d3c486
AnjoC07 <anjocaluya.ama@gmail.com>
2024-02-27 10:24:11 +0800

    build: add image to db  @lib\actions\image.actions.ts

--- lib/actions/image.actions.ts
+++ lib/actions/image.actions.ts
@@ -3,21 +3,31 @@
 import { revalidatePath } from "next/cache";
 import { connectToDatabase } from "../database/mongoose";
 import { handleError } from "../utils";
+import User from "../database/models/user.model";
+import Image from "../database/models/image.model";
 
 // ADD IMAGE TO DATABASE
 export async function addImage({ image, userId, path }: AddImageParams) {
   try {
     await connectToDatabase();
 
+    const author = await User.findById(userId);
+
+    if (!author) {
+      throw new Error("Author not found");
+    }
+
+    const newImage = await Image.create({ ...image, author: author._id });
+
     revalidatePath(path);
 
-    return JSON.parse(JSON.stringify(image));
+    return JSON.parse(JSON.stringify(newImage));
   } catch (error) {
     handleError(error);
   }
 }
 
-// UPADATE IMAGE
+// UPDATE IMAGE
 export async function updateImage({ image, userId, path }: UpdateImageParams) {
   try {
     await connectToDatabase();
