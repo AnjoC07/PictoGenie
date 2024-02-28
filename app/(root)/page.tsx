18b4362f1311d5d101356bb5993819d88921db23
AnjoC07 <anjocaluya.ama@gmail.com>
2024-02-28 15:14:31 +0800

    build: Banner Icon  @app\(root)\page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,7 @@
+import { navLinks } from "@/constants";
+import Link from "next/link";
+import Image from "next/image";
+
 const Home = () => {
   return (
     <>
@@ -5,6 +9,20 @@ const Home = () => {
         <h1 className="home-heading">
           Unleash Your Creative Vision with PictoGenie
         </h1>
+        <ul className="flex-center w-full gap-20">
+          {navLinks.slice(1, 5).map((link) => (
+            <Link
+              key={link.route}
+              href={link.route}
+              className="flex-center flex-col gap-2"
+            >
+              <li className="flex-center w-fit rounded-full bg-white p-4">
+                <Image src={link.icon} alt="image" width={24} height={24} />
+              </li>
+              <p>{link.label}</p>
+            </Link>
+          ))}
+        </ul>
       </section>
     </>
   );
