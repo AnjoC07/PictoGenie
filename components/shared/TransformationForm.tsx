115153c5ba6ad6f9c22d4ce10465d3c2de97c99b
AnjoC07 <anjocaluya.ama@gmail.com>
2024-02-29 14:30:40 +0800

    add: to fill value={field.value}  @components\shared\transformationForm.tsx

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -235,6 +235,7 @@ const TransformationForm = ({
                 onValueChange={(value) =>
                   onSelectFieldHandler(value, field.onChange)
                 }
+                value={field.value}
               >
                 <SelectTrigger className="select-field">
                   <SelectValue placeholder="Select Size" />
