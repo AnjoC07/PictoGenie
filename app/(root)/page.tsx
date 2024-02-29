925a2e6dca9dd2eca08dc2cf738e0da625eddb72
AnjoC07 <anjocaluya.ama@gmail.com>
2024-02-29 11:07:06 +0800

    done: 10-homepage-and-search-pagination

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,17 +1,20 @@
-import { navLinks } from "@/constants";
-import Link from "next/link";
-import Image from "next/image";
 import { Collection } from "@/components/shared/Collection";
+import { navLinks } from "@/constants";
+import { getAllImages } from "@/lib/actions/image.actions";
+import Image from "next/image";
+import Link from "next/link";
 
-const Home = ({ searchParams }: SearchParamProps) => {
+const Home = async ({ searchParams }: SearchParamProps) => {
   const page = Number(searchParams?.page) || 1;
   const searchQuery = (searchParams?.query as string) || "";
+
+  const images = await getAllImages({ page, searchQuery });
 
   return (
     <>
       <section className="home">
         <h1 className="home-heading">
-          Unleash Your Creative Vision with PictoGenie
+          Unleash Your Creative Vision with Imaginify
         </h1>
         <ul className="flex-center w-full gap-20">
           {navLinks.slice(1, 5).map((link) => (
@@ -30,7 +33,12 @@ const Home = ({ searchParams }: SearchParamProps) => {
       </section>
 
       <section className="sm:mt-12">
-        <Collection />
+        <Collection
+          hasSearch={true}
+          images={images?.data}
+          totalPages={images?.totalPage}
+          page={page}
+        />
       </section>
     </>
   );
--- lib/actions/image.actions.ts
+++ lib/actions/image.actions.ts
@@ -8,11 +8,10 @@ import Image from "../database/models/image.model";
 import { redirect } from "next/navigation";
 import { v2 as cloudinary } from "cloudinary";
 
-// PopulateUser
-const populateUser = async (query: any) =>
+const populateUser = (query: any) =>
   query.populate({
     path: "author",
-    model: "User",
+    model: User,
     select: "_id firstName lastName",
   });
 
@@ -24,10 +23,13 @@ export async function addImage({ image, userId, path }: AddImageParams) {
     const author = await User.findById(userId);
 
     if (!author) {
-      throw new Error("Author not found");
+      throw new Error("User not found");
     }
 
-    const newImage = await Image.create({ ...image, author: author._id });
+    const newImage = await Image.create({
+      ...image,
+      author: author._id,
+    });
 
     revalidatePath(path);
 
@@ -45,7 +47,7 @@ export async function updateImage({ image, userId, path }: UpdateImageParams) {
     const imageToUpdate = await Image.findById(image._id);
 
     if (!imageToUpdate || imageToUpdate.author.toHexString() !== userId) {
-      throw new Error("Unauthorized or Image not found");
+      throw new Error("Unauthorized or image not found");
     }
 
     const updatedImage = await Image.findByIdAndUpdate(
@@ -56,7 +58,7 @@ export async function updateImage({ image, userId, path }: UpdateImageParams) {
 
     revalidatePath(path);
 
-    return JSON.parse(JSON.stringify(image));
+    return JSON.parse(JSON.stringify(updatedImage));
   } catch (error) {
     handleError(error);
   }
@@ -82,31 +84,76 @@ export async function getImageById(imageId: string) {
 
     const image = await populateUser(Image.findById(imageId));
 
-    if (!image) {
-      throw new Error("Image not found");
-    }
+    if (!image) throw new Error("Image not found");
 
-    return JSON.parse(JSON.stringify(imageId));
+    return JSON.parse(JSON.stringify(image));
   } catch (error) {
     handleError(error);
   }
+}
 
-  // GET ALL IMAGES
-  export async function getAllImages({
-    limit = 9,
-    page = 1,
-    searchQuery = "",
-  }: {
-    limit?: number;
-    page: number;
-    searchQuery?: string;
-  }) {
-    try {
-      await connectToDatabase();
+// GET ALL IMAGES
+export async function getAllImages({
+  limit = 9,
+  page = 1,
+  searchQuery = "",
+}: {
+  limit?: number;
+  page: number;
+  searchQuery?: string;
+}) {
+  try {
+    await connectToDatabase();
 
-      // Pagination
-    } catch (error) {
-      handleError(error);
+    // Pagination
+    cloudinary.config({
+      cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+      secure: true,
+    });
+
+    let expression = "folder=pictogenie";
+
+    if (searchQuery) {
+      expression += ` AND ${searchQuery}`;
     }
+
+    // Back to resource ID
+    const { resources } = await cloudinary.search
+      .expression(expression)
+      .execute();
+
+    const resourceIds = resources.map((resource: any) => resource.public_id);
+
+    // Querying Own Database
+    let query = {};
+
+    if (searchQuery) {
+      query = {
+        publicId: {
+          $in: resourceIds,
+        },
+      };
+    }
+
+    // Limit of cards per page
+    const skipAmount = (Number(page) - 1) * limit;
+
+    const images = await populateUser(Image.find(query))
+      .sort({ updatedAt: -1 })
+      .skip(skipAmount)
+      .limit(limit);
+
+    const totalImages = await Image.find(query).countDocuments();
+    const savedImages = await Image.find().countDocuments();
+
+    return {
+      data: JSON.parse(JSON.stringify(images)),
+      totalPage: Math.ceil(totalImages / limit),
+      savedImages,
+    };
+  } catch (error) {
+    handleError(error);
   }
 }
