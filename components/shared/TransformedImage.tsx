81861619f42c175ce5673793e4fc07811b8d0e76
AnjoC07 <anjocaluya.ama@gmail.com>
2024-02-29 14:34:57 +0800

    add: self invoke() @debounce @components\shared\transformedImage.tsx

--- components/shared/TransformedImage.tsx
+++ components/shared/TransformedImage.tsx
@@ -65,7 +65,7 @@ const TransformedImage = ({
             onError={() => {
               debounce(() => {
                 setIsTransforming && setIsTransforming(false);
-              }, 8000);
+              }, 8000)();
             }}
             {...transformationConfig}
           />
